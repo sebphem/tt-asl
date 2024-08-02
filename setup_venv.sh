295a405a544049f502bbe56e3857b006bd7627a5
sebastian-phemister <sebastian.phemister@gmail.com>
2024-08-02 14:38:03 -0700

    branch issues

--- setup_venv.sh
+++ setup_venv.sh
@@ -1,5 +1,5 @@
 # run these commands to setup a mini environment for this demo
-python -m venv
+python3 -m venv venv
 source venv/bin/activate
 
 # install all the reqs, you can do this either in the virtual environment or in your global install
