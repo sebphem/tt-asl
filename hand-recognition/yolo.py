5576bf72c8edf9b27cb78724e4bd8dba6ec176c8
Ben Scholar <bscholar@tenstorrent.com>
2024-08-02 11:37:25 -0700

    initial file

---
+++ hand-recognition/yolo.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+
+
+import os, sys
+import math, time
+import argparse
+
+import torch
+import cv2
+
+if __name__ == "__main__":
+    vid = cv2.VideoCapture(0)
+
+    while True:
+        ret, frame = vid.read()
+        cv2.imshow("frame", frame)
+
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+
